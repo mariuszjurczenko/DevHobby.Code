ad091a227a388964a3a47dd763fa5359808e2e99
mariuszjurczenko <mariuszjurczenko@gmail.com>
2025-07-24 16:11:41 +0200

    feat(rpg): Dodaj klasy Bohater i Program – budujemy grę RPG w C# dla początkujących
    Ten commit wprowadza projekt DevHobby.Code.RPG, będący praktycznym przykładem do tutoriala o klasach i obiektach w języku C#.
    W przykładzie tworzymy prostą grę RPG, gdzie bohaterowie (Aragorn, Legolas, Gimli i spółka) posiadają swoje cechy: imię, punkty życia i siłę.
    
    Kod demonstruje:
    
    hermetyzację danych przez prywatne pola i publiczne właściwości,
    
    walidację wartości (np. ograniczenie siły do przedziału 1–100),
    
    inicjalizację obiektów przez konstruktor,
    
    praktyczne zastosowanie tablicy obiektów i pętli foreach.
    
    To doskonały materiał dla początkujących, którzy chcą zrozumieć, czym są klasy, obiekty i jak z nich korzystać przy tworzeniu własnych projektów.

---
+++ DevHobby.Code.RPG/Bohater.cs
@@ -0,0 +1,63 @@
+﻿namespace DevHobby.Code.RPG;
+
+public class Bohater
+{
+    // Pola klasy - cechy każdego bohatera
+    // Pola są teraz PRYWATNE - nikt z zewnątrz nie ma dostępu
+    private string imie;
+    private int puktyZycia;
+    private int sila;
+
+    // Właściwości - wyglądają jak pola, działają jak metody!
+    public string Imie
+    {
+        get { return imie; }
+        private set { imie = value; }
+    }
+
+    public int PunktyZycia
+    {
+        get { return puktyZycia; }
+        set
+        {
+            if (value < 0)
+            {
+                puktyZycia = 0;
+                Console.WriteLine($"{Imie}: HP ustawione na 0 (próbowano: {value})");
+            }
+            else
+            {
+                puktyZycia = value;
+            }
+        }
+    }
+
+    public int Sila
+    {
+        get { return sila; }
+        set
+        {
+            if (value < 1)
+            {
+                sila = 1;
+            }
+            else if (value > 100)
+            {
+                sila = 100;
+            }
+            else
+            {
+                sila = value;
+            }
+        }
+    }
+
+    // Konstruktor - specjalna metoda wywoływana przy tworzeniu obiektu
+    public Bohater(string imie, int puktyZycia, int sila)
+    {
+        // 'this' odnosi się do aktualnego obiektu
+        Imie = imie;
+        PunktyZycia = puktyZycia;
+        Sila = sila;
+    }
+}
---
+++ DevHobby.Code.RPG/Program.cs
@@ -0,0 +1,36 @@
+﻿using DevHobby.Code.RPG;
+
+public class Program
+{
+    static void Main()
+    {
+        // 2 sposób - klasy. Tworzymy pierwszego bohatera
+        Bohater aragorn = new Bohater("Aragorn", 100, 25);
+        Bohater legolas = new Bohater("Legolas", 80, 30);
+        Bohater gimli = new Bohater("Gimli", 120, 35);
+
+        // Możemy nawet stworzyć bohatera "w locie"
+        Bohater[] druzyna = { 
+            new Bohater("Frodo", 50, 10),
+            new Bohater("Sam", 60, 15),
+            new Bohater("Gandalf", 150, 40),
+            aragorn,
+            gimli, 
+            legolas 
+        };
+
+        foreach (var bohater in druzyna)
+        {
+            Console.WriteLine($"{bohater.Imie} - HP: {bohater.PunktyZycia}, Siła: {bohater.Sila}");
+        }
+
+        // Problemy z publicznymi polami
+        aragorn.PunktyZycia = 100;
+        legolas.Sila = 9999;
+        //gimli.Imie = "";
+
+        Console.WriteLine($"{aragorn.Imie} ma {aragorn.PunktyZycia} HP");
+        Console.WriteLine($"{legolas.Imie} ma {legolas.Sila} Sila");
+        Console.WriteLine($"{gimli.Imie} ma {gimli.Sila} Sila");
+    }
+}
